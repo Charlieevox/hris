fa6dcd6aefc8c0630c4732025b6399f6b4783413
charlie <charlie_evolution15@yahoo.com>
2019-08-03 13:28:57 +0700

    Initiate Input

--- config/db.php
+++ config/db.php
@@ -4,6 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=hris',
     'username' => 'root',
-    'password' => 'esbfnb.2018',
+    'password' => '',
+    //'password' => 'esbfnb.2018',
     'charset' => 'utf8',
 ];
--- database_backup/re-work payroll.sql
+++ database_backup/re-work payroll.sql
@@ -11,11 +11,31 @@ CALL spa_workingcalcdate('2019/01');
 -- CALL spa_workingcalctime (periodDate);
 -- CALL spl_loanProcess(periodDate);
 CALL spr_insertPayrollComponent('2019/01');
-CALL spr_jamsostekcacl(periodDate);
-CALL spr_taxcalcIncome(periodDate);
-CALL spr_taxcalctiering(periodDate,1);
+CALL spr_jamsostekcacl('2019/01');
+CALL spr_taxcalcIncome('2019/01');
+CALL spr_taxcalctiering('2019/01',1);
 
-call spr_payrollCalculation ('2019/01');
+CALL spr_payrollCalculation('2019/01');
+CALL spr_payrollCalculation('2019/02');
+CALL spr_payrollCalculation('2019/03');
+CALL spr_payrollCalculation('2019/04');
+CALL spr_payrollCalculation('2019/05');
+CALL spr_payrollCalculation('2019/06');
+CALL spr_payrollCalculation('2019/07');
+CALL spr_payrollCalculation('2019/08');
+CALL spr_payrollCalculation('2019/09');
+CALL spr_payrollCalculation('2019/10');
+CALL spr_payrollCalculation('2019/11');
+CALL spr_payrollCalculation('2019/12');
+
+
+
+
+SELECT * FROM tr_working;
 SELECT * FROM tr_payroll;
+SELECT * FROM tr_payrolltaxincome;
+SELECT * FROM tr_payrolltaxmonthlyproc;
+SELECT * FROM tr_payrolltaxmonthlyproc;
 
-ms_payrollfixdetail
+
+
