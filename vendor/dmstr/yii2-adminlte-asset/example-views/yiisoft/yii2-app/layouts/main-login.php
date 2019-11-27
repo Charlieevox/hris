fac7478608217fbc5d95d1cd18bded2c464ad010
Charlie CS <charlie@esb.co.id>
2019-11-27 16:25:13 +0700

    Initial Input

---
+++ vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/main-login.php
@@ -0,0 +1,35 @@
+<?php
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+dmstr\web\AdminLteAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="login-page">
+
+<?php $this->beginBody() ?>
+
+    <?= $content ?>
+	<footer class="footer">
+        <div class="container-fluid">
+            <p class="pull-left">EasyB v.1</p>
+            <p class="pull-right">PT. Esensi Solusi Buana &copy; <?= date('Y') ?></p>
+        </div>
+    </footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
